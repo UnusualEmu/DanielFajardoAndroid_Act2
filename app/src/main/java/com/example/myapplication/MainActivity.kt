f00078b0813776c17f0d0a50d274162a72cfe313
Jesus Daniel Fajardo <a23jesfajcab@inspedralbes.cat>
2024-10-10 00:50:26 +0200

    Actividad3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 import android.widget.TextView
-
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        val textIUno = findViewById<TextView>(R.id.textOne)
+
+        // Referenciamos los TextViews
+        val textUno = findViewById<TextView>(R.id.textOne)
         val textDos = findViewById<TextView>(R.id.textOne2)
 
-        textIUno.text = "KOTLIN"
-        textDos.text = "HOLA"
+        // Referenciamos el ConstraintLayout
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.main)
 
+        // Obtenemos el color desde colors.xml
+        val color = resources.getColor(R.color.color_fondo, theme) //Usamos el color de xml para esto
 
+        // Referenciamos el botón
+        val button = findViewById<Button>(R.id.button)
+
+        // Configuramos el listener para cambiar el fondo al presionar el botón y el texto desde xml
+        button.setOnClickListener {
+            constraintLayout.setBackgroundColor(color)
+            textUno.text = getString(R.string.text_two)
+            textDos.text = getString(R.string.text_one)
+        }
     }
 }
